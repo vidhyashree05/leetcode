1d657df8eada8f2f65ca4fdd07624f6969a001c4
Vidhya Shree B M <vidhyashreebmvidhya@gmail.com>
2026-01-15 01:44:56 +0530

    Time: 0 ms (100%), Space: 35.7 MB (77.72%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int totalSum = 0;
+        for (int x : nums)
+            totalSum += x;
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (leftSum == totalSum - leftSum - nums[i])
+                return i;
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
