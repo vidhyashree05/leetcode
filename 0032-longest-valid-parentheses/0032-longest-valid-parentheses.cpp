1afdbca09c121bf3d0a8b5de12f3bd450fdc54e7
Vidhya Shree B M <vidhyashreebmvidhya@gmail.com>
2026-01-28 21:10:18 +0530

    Time: 0 ms (100%), Space: 11.8 MB (51.93%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);  
+
+        int maxLen = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if (st.empty()) {
+                    st.push(i);
+                } else {
+                    maxLen = max(maxLen, i - st.top());
+                }
+            }
+        }
+        return maxLen;
+    }
+};
