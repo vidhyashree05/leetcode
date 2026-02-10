c70e3b8aef07ae94ce19e7eca1271544538f4c9f
Vidhya Shree B M <vidhyashreebmvidhya@gmail.com>
2026-02-10 18:31:26 +0530

    Time: 3 ms (31.73%), Space: 9.8 MB (77.14%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            // Skip non-alphanumeric characters
+            while (left < right && !isalnum(s[left])) left++;
+            while (left < right && !isalnum(s[right])) right--;
+
+            // Compare characters
+            if (tolower(s[left]) != tolower(s[right]))
+                return false;
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
