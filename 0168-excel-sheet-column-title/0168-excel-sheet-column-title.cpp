9cb254b32b0ab837e48776f3e800a202a4263425
Vidhya Shree B M <vidhyashreebmvidhya@gmail.com>
2026-02-17 00:42:00 +0530

    Time: 0 ms (100%), Space: 7.6 MB (90.8%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string result;
+
+        while (columnNumber > 0) {
+            columnNumber--;  // Convert to 0-based
+
+            char ch = 'A' + (columnNumber % 26);
+            result = ch + result;
+
+            columnNumber /= 26;
+        }
+
+        return result;
+    }
+};
