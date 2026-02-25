5b10405aaf91ef4a82efcb56cae1d55a3cf93517
Vidhya Shree B M <vidhyashreebmvidhya@gmail.com>
2026-02-26 00:16:32 +0530

    Time: 0 ms (100%), Space: 9.1 MB (94.91%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        
+        vector<string> result;
+        int n = nums.size();
+        
+        if(n == 0) return result;
+        
+        int start = nums[0];  // start of range
+        
+        for(int i = 1; i <= n; i++) {
+            
+            // If end of array OR break in sequence
+            if(i == n || nums[i] != nums[i-1] + 1) {
+                
+                if(start == nums[i-1]) {
+                    result.push_back(to_string(start));
+                } 
+                else {
+                    result.push_back(to_string(start) + "->" + to_string(nums[i-1]));
+                }
+                
+                // Start new range
+                if(i < n)
+                    start = nums[i];
+            }
+        }
+        
+        return result;
+    }
+};
