1ba34a44580eae2483ebe886bbcce740f2390581
Vidhya Shree B M <vidhyashreebmvidhya@gmail.com>
2026-03-01 12:33:11 +0530

    Time: 0 ms (100%), Space: 16.5 MB (53.96%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        int maxDigit = 0;
+        
+        for(char c : n) {
+            maxDigit = max(maxDigit, c - '0');
+        }
+        
+        return maxDigit;
+    }
+};
