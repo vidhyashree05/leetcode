23fc9d6f9467d47f9a8dac78610f425552db7201
Vidhya Shree B M <vidhyashreebmvidhya@gmail.com>
2026-05-03 08:34:48 +0530

    Time: 0 ms (100%), Space: 8.1 MB (84.87%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,10 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) return false;
+        return (s + s).find(goal) != string::npos;
+    }
+};
